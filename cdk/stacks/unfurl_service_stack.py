95c1848539b8308012f35db27724bc5cb4008ff1
William Lane <william.west.lane@gmail.com>
2025-05-28 12:55:49 -0500

    fixing CI deployment workflow

--- cdk/stacks/unfurl_service_stack.py
+++ cdk/stacks/unfurl_service_stack.py
@@ -32,7 +32,9 @@ class UnfurlServiceStack(Stack):
             billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
             removal_policy=RemovalPolicy.DESTROY,
             time_to_live_attribute="ttl",
-            point_in_time_recovery=True,
+            point_in_time_recovery_specification=dynamodb.PointInTimeRecoverySpecification(
+                point_in_time_recovery_enabled=True
+            ),
         )
 
         # SNS topic for async processing
